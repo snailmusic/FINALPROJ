5a60730d622f4d966763d150aaf1ffda71e9756a
Dlol <dravennadas@gmail.com>
2022-04-22 10:49:46 -0500

    scream

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import "./style.css";
+import { mat4 } from "gl-matrix-ts";
+
 import Canvas from "./WebGL/Canvas";
 import Shader from "./WebGL/Shader";
 import { Rectangle } from "./WebGL/Shapes";
 import { Colors } from "./WebGL/Types";
-import { mat4 } from "gl-matrix";
 
 const app = document.querySelector<HTMLDivElement>("#app");
 const canv = new Canvas(640, 480);
 let testRect: Rectangle | null = null;
+
+const projMat = mat4.create();
+mat4.ortho(projMat, 0, canv.c.width, canv.c.height, 0, 0.0, 2);
+
+const viewMat = mat4.create();
+
 interface Shaders {
   basic: null | Shader;
 }
@@ -28,8 +35,8 @@ function init() {
       shaders.basic.addUniformLoc("uProjectionMatrix");
       shaders.basic.addUniformLoc("uColor");
       testRect = new Rectangle(
-        { x: -1, y: -1 },
-        { x: 0.5, y: 0.5 },
+        { x: 0, y: 0 },
+        { x: 100, y: 100 },
         canv,
         Colors.red,
         shaders.basic
@@ -51,12 +58,27 @@ function update(_delta: DOMHighResTimeStamp) {
 function draw() {
   const { gl } = canv;
 
+  const progInfo = shaders.basic?.programInfo;
+
   gl?.clearColor(0, 0, 0, 1.0); // Clear to black, fully opaque
   gl?.clearDepth(1.0); // Clear everything
 
   gl?.clear(gl?.COLOR_BUFFER_BIT | gl?.DEPTH_BUFFER_BIT);
 
-  testRect?.draw()
+  const modelMat = mat4.create();
+  mat4.translate(modelMat, modelMat, [0,0,0])
+
+  shaders.basic?.bind();
+  gl?.uniform4fv(
+    progInfo?.uniformLocations.uViewMatrix,
+    viewMat
+  );
+  gl?.uniform4fv(progInfo?.uniformLocations.uViewMatrix, viewMat);
+  gl?.uniform4fv(progInfo?.uniformLocations.uProjMatrix, projMat);
+  gl?.uniform4fv(progInfo?.uniformLocations.uModelMatrix, modelMat);
+  gl?.uniform4f(progInfo?.uniformLocations.uColor, 1, 0, 1, 1);
+
+  testRect?.draw();
 }
 
 window.onload = init;
