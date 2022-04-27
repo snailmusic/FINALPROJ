5a7037d01f32d89fa8af548f6127b49f3ba6304a
Dlol <dravennadas@gmail.com>
2022-04-26 20:00:59 -0500

    im gonna work on something else

--- src/Bullet.ts
+++ src/Bullet.ts
@@ -11,7 +11,7 @@ export default class Bullet extends GameObject {
   canvas: Canvas;
   shader: Shader;
   constructor(pos: Vec2, canvas: Canvas, shader: Shader, angle: number) {
-    const size: Vec2 = { x: 64, y: 64 };
+    const size: Vec2 = { x: 16, y: 16 };
     super(pos, size);
     this.canvas = canvas;
     this.shader = shader;
@@ -29,9 +29,9 @@ export default class Bullet extends GameObject {
     const { gl } = this.canvas;
     const programInfo = this.shader.programInfo;
     const model = mat4.create();
-	const speed = 10;
+    const speed = 10;
     this.pos.x += Math.cos(this.angle) * speed;
-	this.pos.y += Math.sin(this.angle) * speed;
+    this.pos.y += Math.sin(this.angle) * speed;
     mat4.translate(model, model, [this.pos.x, this.pos.y, 0]);
     gl?.uniformMatrix4fv(
       programInfo?.uniformLocations.uModelMatrix,
--- src/Enemy.ts
+++ src/Enemy.ts
@@ -1,3 +1,31 @@
+import Bullet from "./Bullet";
 import GameObject from "./GameObject";
+import Canvas from "./WebGL/Canvas";
+import Shader from "./WebGL/Shader";
+import { Vec2 } from "./WebGL/Types";
 
-export default class Enemy extends GameObject {}
+enum PatternType {
+  Spiral,
+  Radial,
+  Focused,
+}
+
+export default class Enemy extends GameObject {
+  type: PatternType;
+  canvas: Canvas;
+  shader: Shader;
+  bullets: Bullet[];
+  constructor(pos: Vec2, type: PatternType, canvas: Canvas, shader: Shader) {
+    super(pos, { x: 64, y: 64 });
+    this.type = type;
+    this.canvas = canvas;
+    this.shader = shader;
+    this.bullets = [];
+  }
+
+  draw(): void {
+      for (const bullet of this.bullets) {
+          bullet.draw();
+      }
+  }
+}
